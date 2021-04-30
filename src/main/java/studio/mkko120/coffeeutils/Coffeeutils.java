0f60f1a70fe9feb4054d3dc5c2c755d8486b6421
mkko120 <mrmikillll@gmail.com>
2021-04-30 20:29:58 +0200

    Init

---
+++ src/main/java/studio/mkko120/coffeeutils/Coffeeutils.java
@@ -0,0 +1,17 @@
+package studio.mkko120.coffeeutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Coffeeutils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
